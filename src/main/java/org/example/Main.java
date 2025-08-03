c15aa8c90fc0fd69135101497ed05d9c36405ed8
Jake Adkirson <jikjaklol363@gmail.com>
2025-08-03 15:50:08 -0500

    Goodbye Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Goodbye, World!");
+        System.out.print("Goodbye, World!");
     }
 }
